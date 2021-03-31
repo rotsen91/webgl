4ad095aedbcbef841f06c00efd8dabcf7fb75780
nestor.morales2@upr.edu <nestor.morales2@upr.edu>
2021-03-31 15:37:28 -0400

    setting screen height

--- js.js
+++ js.js
@@ -4,24 +4,27 @@ function red() {
 }
 
 function orange() {
-    $('div').css('background','#EBAE10')
+    $('canvas').css('height', screen.height - 200)
     .text('Screen Size ORANGE');
 }
 
 function green() {
-    $('div').css('background','#83ba2b')
-    .text('Screen Size GREEN');
+    $('canvas').css('height', screen.height - 200)
+    .text('Screen Size GREEN')
 }
 
 
 function resizeFn() {
+
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
+    
+    
     orange()
     }else{
     // false for not mobile device
     green()
     }
 }
-window.onresize = resizeFn;
-resizeFn();
+// window.onresize = resizeFn;
+// resizeFn();
